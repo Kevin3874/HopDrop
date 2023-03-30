814cdd6ca1ff75cda45f45a4487260e985cf122b
Kevin3874 <kzhang77@jh.edu>
2023-03-29 21:53:09 -0400

    Update gradle and theme. Login design changes

--- app/src/main/java/com/example/a5_sample/LoginActivity.java
+++ app/src/main/java/com/example/a5_sample/LoginActivity.java
@@ -23,16 +23,11 @@ public class LoginActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         SharedPreferences myPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         String name = myPrefs.getString("loginName", "Owner");
-        EditText login_name = findViewById(R.id.login_name);
-        login_name.setText(name);
 
         //when user clicks to login
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences.Editor myEdit = myPrefs.edit();
-                myEdit.putString("loginName", login_name.getText().toString());
-                myEdit.apply();
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
