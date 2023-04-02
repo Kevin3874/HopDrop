59f42bf23343338da808d1be3d0abe3367ddb48a
Vivian Liu <vivianliu@MacBook-Pro-3.local>
2023-04-02 00:36:04 -0400

    Added table to profile page

--- app/src/main/java/com/example/a5_sample/LoginActivity.java
+++ app/src/main/java/com/example/a5_sample/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.example.a5_sample;
 
+import static java.security.AccessController.getContext;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -18,10 +24,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_screen);
 
         Button login_button = findViewById(R.id.login_button);
+        //Gets username text box
+        TextInputLayout username = findViewById(R.id.username);
+        //Gets password text box
+        TextInputLayout password = findViewById(R.id.password);
 
         //save information
         Context context = getApplicationContext();
         SharedPreferences myPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        //This would probably only be if we wanted to auto-populate the username box with previous login
         String name = myPrefs.getString("loginName", "Owner");
 
         //when user clicks to login
@@ -29,8 +40,20 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                SharedPreferences.Editor editor = myPrefs.edit();
+                //Currently only autopopulates/saves the email that was entered as the username
+                //Need to figure out how to get the actual name of the person from the email (probably using the database)
+                if (!TextUtils.isEmpty(username.getEditText().getText().toString())) {
+                    String username_string = username.getEditText().getText().toString();
+                    editor.putString("loginName", username_string);
+                    editor.apply();
+                    startActivity(intent);
+                    finish();
+                } else {
+                    //Creates a toast if username isn't filled in
+                    //Still need to do this for the password
+                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
