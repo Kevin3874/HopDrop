ecf2cf9cb42b494174021c50e69a1c9ca5277eb3
Vivian Liu <vivianliu@MacBook-Pro-5.local>
2023-04-06 23:22:46 -0400

    Added order progress activity

---
+++ app/src/main/java/com/example/HopDrop/OrderProgress.java
@@ -0,0 +1,15 @@
+package com.example.HopDrop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class OrderProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_progress);
+    }
+}
