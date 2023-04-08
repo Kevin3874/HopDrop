872a04632d51c16a3c5b3a84827d4c8209a7f752
Vivian Liu <vivianliu@macbook-pro-6.acasmart.jh.edu>
2023-04-07 23:55:09 -0400

    Updated order progress page

--- app/src/main/java/com/example/HopDrop/OrderProgress.java
+++ app/src/main/java/com/example/HopDrop/OrderProgress.java
@@ -4,12 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.shuhart.stepview.StepView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderProgress extends AppCompatActivity {
+    StepView progress_bar;
+    List<String> steps = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_progress);
+        progress_bar = findViewById(R.id.step_view);
+        progress_bar.setStepsNumber(3);
+        progress_bar.setSteps(steps);
+
     }
 }
