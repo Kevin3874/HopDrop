823128059e9ca3d818a04ff4d4c98a598f196a13
Kevin3874 <kzhang77@jh.edu>
2023-04-16 22:17:36 -0400

    Added order firebase storage in real-time

--- app/src/main/java/com/example/HopDrop/ui/home/ViewPagerFragment.java
+++ app/src/main/java/com/example/HopDrop/ui/home/ViewPagerFragment.java
@@ -1,19 +1,13 @@
 package com.example.HopDrop.ui.home;
 
-import static com.example.HopDrop.LoginActivity.username_string;
-import static com.example.HopDrop.MainActivity.orders;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,30 +17,18 @@ import com.example.HopDrop.OrderAdapter;
 import com.example.HopDrop.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.Filter;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.model.Document;
-
-import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 public class ViewPagerFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
     private OrderAdapter mOrderAdapter;
-    FirebaseFirestore rootRef = FirebaseFirestore.getInstance();;
+    FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
     //private List<Order> orders;
     private String title;
     private String tab;
@@ -77,21 +59,27 @@ public class ViewPagerFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_order, container, false);
 
         // Get the list of orders
-
-        List<Order> test = EventChangeListener();
-        test.add(new Order("TestHome", "New York", "Los Angeles", "500.00", "I am Brody cafe", "ai7s7dyg"));
-        // Set up the RecyclerView and adapter
-        mRecyclerView = view.findViewById(R.id.recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mOrderAdapter = new OrderAdapter(test, tab);
-        mRecyclerView.setAdapter(mOrderAdapter);
-
-
+        EventChangeListener(new OnOrdersFetchedListener() {
+            @Override
+            public void onOrdersFetched(List<Order> orders) {
+                if (orders != null) {
+                    // Set up the RecyclerView and adapter
+                    mRecyclerView = view.findViewById(R.id.recycler_view);
+                    mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    mOrderAdapter = new OrderAdapter(orders, tab);
+                    mRecyclerView.setAdapter(mOrderAdapter);
+                }
+            }
+        });
 
         return view;
     }
 
-    private List<Order> EventChangeListener() {
+    public interface OnOrdersFetchedListener {
+        void onOrdersFetched(List<Order> orders);
+    }
+
+    private void EventChangeListener(final OnOrdersFetchedListener listener) {
         List<Order> orders = new ArrayList<>();
         rootRef.collection("orders")
                 .get()
@@ -100,64 +88,14 @@ public class ViewPagerFragment extends Fragment {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             Log.d("Test", "test");
-                            for (QueryDocumentSnapshot document : task.getResult()) {
+                            for (DocumentSnapshot document : task.getResult()) {
                                 Order order = document.toObject(Order.class);
                                 orders.add(order);
                                 Log.d("Order array", String.valueOf(orders));
                             }
+                            listener.onOrdersFetched(orders);
                         }
                     }
                 });
-        return orders;
-
-        //retrieve data
-
-        /*
-        rootRef.collection("orders").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                for (QueryDocumentSnapshot dc : task.getResult()) {
-                    orders.add(new Order("Not determined yet", (String) dc.get("fromLocation"), (String) dc.get("dest"), (String) dc.get("fee"), (String) dc.get("notes"), (String) dc.get("orderID")));
-                }
-            }
-        });
-        */
-
-        /*
-        rootRef.collection("orders").addSnapshotListener(new EventListener<QuerySnapshot>() {
-
-
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-
-                if (error != null) {
-                    Log.e("Firestore error", error.getMessage());
-                    return;
-                }
-
-                for (DocumentChange dc : value.getDocumentChanges()) {
-                    //HashMap<String, Order> map = (HashMap<String, Order>) dc.getDocument().getData();
-
-                    if (dc.getType() == DocumentChange.Type.ADDED)  {
-                        //orders.add(dc.getDocument().toObject(Order.class));
-                        //orders.add((Order) dc.getDocument().get("orders"));
-                        //orders.add(new Order((String) dc.getDocument().get("customer_name"), (String) dc.getDocument().get("fromLocation"), (String) dc.getDocument().get("dest"), (String) dc.getDocument().get("fee"), (String) dc.getDocument().get("notes"), (String) dc.getDocument().get("orderID")));
-                        //String customerName = map.get("customer_name");
-                        //String destination = (String) map.get("dest");
-                        //String fee = (String) map.get("fee");
-                        //String fromLocation = (String) map.get("fromLocation");
-                        //String notes = (String) map.get("notes");
-                        //String orderID = (String) map.get("orderID");
-                        //orders.add(new Order(customerName, destination, fee, fromLocation, notes, orderID));
-                        //System.out.println(map.toString());
-                    }
-                    mOrderAdapter.notifyDataSetChanged();
-                }
-
-            }
-        });
-
-             */
-
     }
 }
--- app/src/main/java/com/example/HopDrop/ui/order/OrderFragment.java
+++ app/src/main/java/com/example/HopDrop/ui/order/OrderFragment.java
@@ -1,10 +1,12 @@
 package com.example.HopDrop.ui.order;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,11 +14,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.HopDrop.Order;
 import com.example.HopDrop.OrderAdapter;
 import com.example.HopDrop.R;
+import com.example.HopDrop.ui.home.ViewPagerFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class OrderFragment extends Fragment {
+    FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
     private RecyclerView mRecyclerView;
     private OrderAdapter mOrderAdapter;
 
@@ -26,18 +35,46 @@ public class OrderFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_order, container, false);
 
         // Get the list of orders
-        List<Order> orders = getOrders();
-
-        // Set up the RecyclerView and adapter
-        mRecyclerView = view.findViewById(R.id.recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mOrderAdapter = new OrderAdapter(orders, "orders");
-        mRecyclerView.setAdapter(mOrderAdapter);
+        EventChangeListener(new ViewPagerFragment.OnOrdersFetchedListener() {
+            @Override
+            public void onOrdersFetched(List<Order> orders) {
+                if (orders != null) {
+                    // Set up the RecyclerView and adapter
+                    mRecyclerView = view.findViewById(R.id.recycler_view);
+                    mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    mOrderAdapter = new OrderAdapter(orders, "orders");
+                    mRecyclerView.setAdapter(mOrderAdapter);
+                }
+            }
+        });
 
         return view;
     }
+    public interface OnOrdersFetchedListener {
+        void onOrdersFetched(List<Order> orders);
+    }
 
+    private void EventChangeListener(final ViewPagerFragment.OnOrdersFetchedListener listener) {
+        List<Order> orders = new ArrayList<>();
+        rootRef.collection("orders")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("Test", "test");
+                            for (DocumentSnapshot document : task.getResult()) {
+                                Order order = document.toObject(Order.class);
+                                orders.add(order);
+                                Log.d("Order array", String.valueOf(orders));
+                            }
+                            listener.onOrdersFetched(orders);
+                        }
+                    }
+                });
+    }
     // Returns a list of dummy orders
+    /*
     private List<Order> getOrders() {
         List<Order> orders = new ArrayList<>();
         //orders.add(new Order("John Smith", "New York", "Los Angeles", 500.00f, "please give me a call when arrived"));
@@ -45,4 +82,6 @@ public class OrderFragment extends Fragment {
         //orders.add(new Order("Bob Johnson", "Boston", "Chicago", 250.00f, "ketchup and fork please", "asd"));
         return orders;
     }
+
+     */
 }
